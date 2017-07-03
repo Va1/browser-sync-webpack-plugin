60a07cca3bf23c5c3fe111b6f98a07ca0472f8fd
Valentyn <valpreacher@gmail.com>
2017-07-03 22:44:17 +0300

    not showing 'Rebuilding...' notification when 'notify' is false

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ BrowserSyncPlugin.prototype.apply = function (compiler) {
   });
 
   compiler.plugin('compilation', function () {
-    if (self.isBrowserSyncRunning) {
+    if (self.isBrowserSyncRunning && self.browserSyncOptions.notify) {
       self.browserSync.notify('Rebuilding...');
     }
   });
